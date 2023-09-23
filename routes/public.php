2f6f281b506137626fcedc13ec7cbb0b08ad625b
irpannawawi <irpannawawi.ixd@gmail.com>
2023-09-23 19:34:14 +0700

    fix routing not found on create article

--- routes/public.php
+++ routes/public.php
@@ -9,4 +9,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [WebController::class, 'index']);
 Route::get('/single-post', [WebController::class, 'singlePost']);
 Route::get('/c/{rubrik}', [WebController::class, 'showCategory'])->name('showCategory');
-Route::get('/{rubrik}/{post_id}', [WebController::class, 'singlePost'])->name('singlePost');
+Route::get('/p/{rubrik}/{post_id}', [WebController::class, 'singlePost'])->name('singlePost');

e54a18156480af72f2ffa6fb226637cce42eda90
sandemoid <infosandemo@gmail.com>
2023-11-02 17:32:26 +0700

    add: clear cache

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // Menjalankan command 'cache:clear' setiap hari pada pukul 03:00 AM
+        $schedule->command('cache:clear')->dailyAt('03:00');
     }
 
     /**
@@ -25,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }

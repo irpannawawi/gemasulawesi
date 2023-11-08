0e5f6fc6d91e0111cb5c059e1849098e2b0714de
irpannawawi <irpannawawi.ixd@gmail.com>
2023-11-09 03:59:11 +0700

    modified gitignore

--- python/main.py
+++ python/main.py
@@ -3,12 +3,19 @@ import task
 import multiprocessing as mp
 
 url_local = 'https://demo.sandemoindoteknologi.co.id'
-number_of_page = 1 # jumlah halaman isi 100 post 
+page_start = 1 # jumlah halaman isi 100 post 
+page_end = 4  
 
-
+proccess_list = []
+lock = mp.Lock()
 if __name__ == '__main__':
-    n=1
-    while n<=number_of_page:
-        mp.Process(target=task.do_task, args=(100,n, url_local,)).start()
-        n+=1
+
+    while page_start<=page_end:
+        p = mp.Process(target=task.do_task, args=(100,page_start, url_local, lock,))
+        p.start()
+        proccess_list.append(p)
+        page_start+=1
+
+    for p in proccess_list:
+        p.join()
 

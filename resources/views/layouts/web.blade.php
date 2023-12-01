bf1bb520fdef01f487e8319d4bd3f8f66da9f2f1
sandemoid <infosandemo@gmail.com>
2023-12-01 14:00:47 +0700

    fix footer

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -649,23 +649,20 @@
                                 </p>
                             </div>
                             <div class="social__footer socials--medium socials--rounded">
-                                <a class="social social-facebook"
-                                    href="https://web.facebook.com/{{ get_setting('facebook') }}" target="_blank"
-                                    aria-label="facebook">
+                                <a class="social social-facebook" href="https://{{ get_setting('facebook') }}"
+                                    target="_blank" aria-label="facebook">
                                     <i class="fa-brands fa-facebook"></i>
                                 </a>
-                                <a class="social social-twitter" href="https://twitter.com/{{ get_setting('x') }}"
+                                <a class="social social-twitter" href="https://{{ get_setting('x') }}"
                                     target="_blank" aria-label="twitter">
                                     <i class="fa-brands fa-square-x-twitter"></i>
                                 </a>
-                                <a class="social social-youtube"
-                                    href="https://www.youtube.com/channel/{{ get_setting('youtube') }}"
+                                <a class="social social-youtube" href="https://{{ get_setting('youtube') }}"
                                     target="_blank" aria-label="youtube">
                                     <i class="fa-brands fa-youtube"></i>
                                 </a>
-                                <a class="social social-instagram"
-                                    href="https://www.instagram.com/{{ get_setting('instagram') }}" target="_blank"
-                                    aria-label="instagram">
+                                <a class="social social-instagram" href="https://{{ get_setting('instagram') }}"
+                                    target="_blank" aria-label="instagram">
                                     <i class="fa-brands fa-square-instagram"></i>
                                 </a>
                             </div>

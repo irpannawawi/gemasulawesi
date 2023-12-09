30d12bf8b58d17a29da6623148f8f9eb2866844e
irpannawawi <irpannawawi.ixd@gmail.com>
2023-12-10 04:28:34 +0700

    sitemap generator done

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -3,18 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Posts;
+use App\Models\Rubrik;
+use App\Models\Topic;
 use Illuminate\Http\Request;
 use Spatie\Sitemap\Sitemap;
 use Spatie\Sitemap\SitemapGenerator;
-
+use Spatie\Sitemap\Tags\Url;
 
 class SitemapController extends Controller
 {
     public function generate()
     {
-        
-        Sitemap::create()
-        ->add(Posts::all())
+        $res = Sitemap::create()
+        ->add(Url::create(config('app.url')))
+        ->add(Rubrik::all())
+        ->add(Topic::all())
+        ->add(Posts::where('status', 'published')->orderBy('published_at', 'desc')->get())
         ->writeToFile(public_path('sitemap.xml'));
+        dd($res);
     }
 }

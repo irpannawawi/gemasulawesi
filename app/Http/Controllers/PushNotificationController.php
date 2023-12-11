41382bc0d3179cf5cbf6811b8b5ac5b1811f2823
irpannawawi <irpannawawi.ixd@gmail.com>
2023-12-11 22:06:17 +0700

    fixed firebase issue

--- app/Http/Controllers/PushNotificationController.php
+++ app/Http/Controllers/PushNotificationController.php
@@ -6,6 +6,8 @@ use App\Models\Posts;
 use App\Models\PushNotification;
 use App\Models\Subscriber;
 use Illuminate\Http\Request;
+use Kreait\Firebase\Messaging\WebPushConfig;
+use Kreait\Firebase\Messaging\CloudMessage;
 
 class PushNotificationController extends Controller
 {
@@ -31,50 +33,23 @@ class PushNotificationController extends Controller
     public function send(Request $request, $id){
 
         $news = PushNotification::find($id);
-        $url = 'https://fcm.googleapis.com/fcm/send';
         $FcmToken = Subscriber::whereNotNull('token')->pluck('token')->all();
-        $serverKey = 'AAAAeyFuJF0:APA91bETiDiOkQojH4yqYMF68XxJsmQyLW3lZTROeKx2s38M119llaDmNYE43Zcm3ragLN6ZXW363WUgBQ5BmaQ_pStK5kBB4YkFK-OXWPgRc1OL7sfCqTzVZe9-0VGg9W-ib_-xQ595';
-  
-        $data = [
-            "registration_ids" => $FcmToken,
-            // "notification" => [
-            //     "click_action" => str_replace('http://', 'https://', $news->url),  
-            // ],
+
+        $messaging = app('firebase.messaging');
+
+        $config = WebPushConfig::fromArray([
             "data"=>[
                 "title" => $news->title,
                 "body" => $news->body,  
                 "click_action" => str_replace('http://', 'https://', $news->url),  
                 "message_id" => date('YmdHis'),
             ]
-        ];
-        // dd($data);
-        $encodedData = json_encode($data);
-    
-        $headers = [
-            'Authorization:key=' . $serverKey,
-            'Content-Type: application/json',
-        ];
-    
-        $ch = curl_init();
-      
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-        // Disabling SSL Certificate support temporarly
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);        
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
-        // Execute post
-        $result = curl_exec($ch);
-        if ($result === FALSE) {
-            die('Curl failed: ' . curl_error($ch));
-        }        
-        // Close connection
-        curl_close($ch);
-        // FCM response
-        // dd($result);
+        ]);
+
+        $message = CloudMessage::new();
+        $message = $message->withWebPushConfig($config);
+        $FcmToken = Subscriber::whereNotNull('token')->pluck('token')->all();
+        $messaging->sendMulticast($message, $FcmToken);
         $news->status='sent';
         $news->save();
         return redirect()->back(); 

0625f77b1a4752e3ca77eef5687223d8dbab600a
irpannawawi <irpannawawi.ixd@gmail.com>
2023-12-18 06:47:37 +0700

    fix layout issue at login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -49,6 +49,7 @@
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('login') }}" id="loginForm">
+        <h3 class="text-center">Login</h3>
         @csrf
 
         <!-- Email Address -->
@@ -57,11 +58,11 @@
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
                 required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-input-error :messages="$errors->get('email')" class="mt-1" />
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
+        <div class="mt-2">
             <x-input-label for="password" :value="__('Password')" />
 
             <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ Storage::url('logo/') . get_setting('logo_web') }}" alt="Logo">
+<img src="{{ Storage::url('logo/') . get_setting('logo_web') }}" alt="Logo" class="img img-responsive" style="max-width: 240px;">

5ca9f7ae3abaf63d1093feb26668f9f58757a5bf
irpannawawi <irpannawawi.ixd@gmail.com>
2023-12-18 08:08:15 +0700

    auto backup to s3 bucket

---
+++ app/Console/Commands/Backup.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Storage;
+
+class Backup extends Command
+{
+    protected $signature = 'backup:daily';
+    protected $description = 'Perform daily backup to S3';
+
+    public function handle()
+    {
+        $backupPath = storage_path('app/backup');
+        $backupFilename = 'backup_' . Carbon::now()->format('Y-m-d_H-i-s') . '.zip';
+
+        // dump mysql 
+        $dumpFile = storage_path('app').'/database.sql';
+        $command = "mysqldump -h ".env('DB_HOST')." -u ".env('DB_USERNAME')." -p".env('DB_PASSWORD')." ".env('DB_DATABASE')." > ".$dumpFile;
+        shell_exec($command);
+        dd('p');
+        // Proses backup (contoh backup direktori storage)
+        $command = "7z a -tzip $backupFilename " . storage_path('app');
+        shell_exec($command);
+
+        // Upload backup ke S3
+        $s3Path = 'backup/' . $backupFilename;
+        $res = Storage::disk('s3')->put($s3Path, file_get_contents($backupFilename));
+        // Hapus file backup lokal jika diinginkan
+        unlink($backupFilename);
+
+        $this->info('Daily backup completed successfully.');
+    }
+}

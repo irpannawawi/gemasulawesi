58ccfae450db9e1fd7882adfcd59685119fc85b1
irpannawawi <irpannawawi.ixd@gmail.com>
2023-12-19 17:31:23 +0700

    alert size

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -45,7 +45,7 @@ class AdController extends Controller
                 $data['title'] = "Below Heading";
                 break;
             case 'content':
-                $data['title'] = "Content";
+                $data['title'] = "Content <small class=\"text-danger\">Gunakan gambar dengan ukuran 600x350px";
                 break;
             case 'below_content':
                 $data['title'] = "Below Content";

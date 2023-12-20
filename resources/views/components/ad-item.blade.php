326843b513fe3c2afb365c6cc79ea85286b53a6f
sandemoid <infosandemo@gmail.com>
2023-12-20 10:15:25 +0700

    fix ads style

--- resources/views/components/ad-item.blade.php
+++ resources/views/components/ad-item.blade.php
@@ -1,30 +1,47 @@
 @php
-    if ($num=='' || empty($num)) {
+    if ($num == '' || empty($num)) {
         $ads = App\Models\Ad::where('position', $position)->get();
     }
 
-    if($num == '0')
-    {
-        $ads = App\Models\Ad::where('position', $position)->limit(1)->get();
+    if ($num == '0') {
+        $ads = App\Models\Ad::where('position', $position)
+            ->limit(1)
+            ->get();
     }
 
-    if ($num=='1') {
+    if ($num == '1') {
         $ads = App\Models\Ad::where('position', $position)->get();
-        if($ads->count()==2){
+        if ($ads->count() == 2) {
             $ads = $ads->skip(1);
-        }else{
+        } else {
             $ads = [];
         }
     }
 
-    
-    
 @endphp
+@push('custom-css')
+    <style>
+        .ads__banner {
+            padding-bottom: 1.5rem;
+        }
 
+        .ads {
+            position: relative;
+            border: 1px solid #eff0f6;
+            box-shadow: 0 2px 8px rgba(0, 0, 0, 0.02);
+            border-radius: 5px;
+        }
+
+        .ads__box {
+            text-align: center;
+            position: relative;
+        }
+    </style>
+@endpush
 
 @foreach ($ads as $ad)
     @if ($ad->type == 'img')
-        <div class="ads shadow p-2 mb-2 mt-2 bg-body rounded" style="width: 100%;">
+        <div class="ads p-2 mb-2 mt-2 bg-body" style="width: 100%;">
             <div class="ads__box">
                 <div class="div-gpt-ad-giant">
                     <a href="{{ $ad->link == '' ? '#' : $ad->link }}" target="__blank">
---
+++ resources/views/frontend/author.blade.php
@@ -0,0 +1,125 @@
+@extends('layouts.web')
+@section('content')
+    <div class="main-container container" id="main-container">
+
+        <!-- Content -->
+        <div class="row row-20">
+
+            <!-- slider -->
+            <div class="col-lg-8 order-lg-2">
+                <x-ad-item position='top_page' />
+
+                <section>
+                    <!-- #TagName -->
+                    <div class="berita-terkini">
+                        <div class="title-list-berita">
+                            <span>Penulis: {{ '-' }}</span>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <ul class="post-list-small post-list-small--2 mb-32">
+                                    @if (!empty($beritaTerkini[0]))
+                                        @foreach ($beritaTerkini[0] as $post)
+                                            <li class="post-list-small__item">
+                                                <article class="post-list-small__entry clearfix">
+                                                    <div class="post__img">
+                                                        <a
+                                                            href="{{ route('singlePost', [
+                                                                'rubrik' => Str::slug($post->rubrik->rubrik_name),
+                                                                'post_id' => $post->post_id,
+                                                                'slug' => $post->slug,
+                                                            ]) }}">
+                                                            <img data-src="{{ get_post_image($post->post_id) }}"
+                                                                src="{{ url('assets/frontend') }}/img/empty.jpg"
+                                                                alt="{{ $post->title }}" class="lazyload">
+                                                        </a>
+                                                    </div>
+                                                    <div class="post-list-small__body">
+                                                        <ul class="entry__meta category underline">
+                                                            <li>
+                                                                <a href="{{ route('category', ['rubrik_name' => Str::slug($post->rubrik->rubrik_name)]) }}"
+                                                                    class="entry__meta-category">{{ $post->rubrik->rubrik_name }}</a>
+                                                            </li>
+                                                        </ul>
+                                                        <h3 class="post-list-small__entry-title">
+                                                            <a
+                                                                href="{{ route('singlePost', [
+                                                                    'rubrik' => Str::slug($post->rubrik->rubrik_name),
+                                                                    'post_id' => $post->post_id,
+                                                                    'slug' => $post->slug,
+                                                                ]) }}">{{ $post->title }}</a>
+                                                        </h3>
+                                                        <p class="bt__date">{{ convert_date_to_ID($post->published_at) }}
+                                                        </p>
+                                                    </div>
+                                                </article>
+                                            </li>
+                                        @endforeach
+                                    @endif
+                                </ul>
+
+                                <!-- Ad Banner 728 -->
+                                {{-- <div class="text-center pb-48">
+                                    <a href="#">
+                                        <img src="{{ url('assets/frontend') }}/img/content/placeholder_728.jpg"
+                                            alt="">
+                                    </a>
+                                </div> --}}
+
+                                <ul class="post-list-small post-list-small--2 mb-32">
+                                    @if (!empty($beritaTerkini[1]))
+                                        @foreach ($beritaTerkini[1] as $post)
+                                            <li class="post-list-small__item">
+                                                <article class="post-list-small__entry clearfix">
+                                                    <div class="post__img">
+                                                        <a
+                                                            href="{{ route('singlePost', [
+                                                                'rubrik' => Str::slug($post->rubrik->rubrik_name),
+                                                                'post_id' => $post->post_id,
+                                                                'slug' => $post->slug,
+                                                            ]) }}">
+                                                            <img data-src="{{ get_post_image($post->post_id) }}"
+                                                                src="{{ url('assets/frontend') }}/img/empty.jpg"
+                                                                alt="{{ $post->title }}" class="lazyload">
+                                                        </a>
+                                                    </div>
+                                                    <div class="post-list-small__body">
+                                                        <ul class="entry__meta category underline">
+                                                            <li>
+                                                                <a href="{{ route('category', ['rubrik_name' => Str::slug($post->rubrik->rubrik_name)]) }}"
+                                                                    class="entry__meta-category">{{ $post->rubrik->rubrik_name }}</a>
+                                                            </li>
+                                                        </ul>
+                                                        <h3 class="post-list-small__entry-title">
+                                                            <a
+                                                                href="{{ route('singlePost', [
+                                                                    'rubrik' => Str::slug($post->rubrik->rubrik_name),
+                                                                    'post_id' => $post->post_id,
+                                                                    'slug' => $post->slug,
+                                                                ]) }}">{{ $post->title }}</a>
+                                                        </h3>
+                                                        <p class="bt__date">{{ convert_date_to_ID($post->published_at) }}
+                                                        </p>
+                                                    </div>
+                                                </article>
+                                            </li>
+                                        @endforeach
+                                    @endif
+                                </ul>
+                                {{ $paginatedPost->onEachSide(1)->links() }}
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            </div> <!-- end slider -->
+
+            <!-- Sidebar -->
+            <x-sidebar />
+            <!-- end sidebar -->
+        </div> <!-- end content -->
+        <div class="col-lg-8">
+            <x-ad-item position='footer' />
+        </div>
+    </div> <!-- end main container -->
+@endsection

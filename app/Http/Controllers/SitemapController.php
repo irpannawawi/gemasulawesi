761f3d2b80df4bbf3cc29fa1400fb5ccab96451e
irpannawawi <irpannawawi.ixd@gmail.com>
2023-12-22 19:06:49 +0700

    sitemaps

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Posts;
 use App\Models\Amp;
 use App\Models\Rubrik;
+use App\Models\Tags;
 use App\Models\Topic;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -18,22 +19,44 @@ class SitemapController extends Controller
 {
     public function generate()
     {
-        $this->create_web_post();
-        $this->create_news_post();
-
         $sitemap = Sitemap::create();
+        $sitemap->add(config('app.url'));
+        $sitemap->add(config('app.url').'/tentang-kami');
+        $sitemap->add(config('app.url').'/kode-etik');
+        $sitemap->add(config('app.url').'/redaksi');
+        $sitemap->add(config('app.url').'/kode-prilaku-pers');
+        $sitemap->add(config('app.url').'/perlindungan-data-pengguna');
+        $sitemap->add(config('app.url').'/gallery');
+
+        //posts sitemaps
+        $this->create_amps();
+        $this->create_amps();
+        
+        $rubriks = Rubrik::all();
+        foreach ($rubriks as $rubrik) {
+            $sitemap->add('storage/sitemaps/' . Str::slug($rubrik->rubrik_name) . '/sitemap_web.xml');
+            $sitemap->add('storage/sitemaps/amp/' . Str::slug($rubrik->rubrik_name) . '/sitemap_web.xml');
+        }
+
+        // rubrik sitemaps
+        
+        // tags sitemaps
+        $this->create_tags();
+        
         $rubriks = Rubrik::all();
         foreach ($rubriks as $rubrik) {
             $sitemap->add('storage/sitemaps/' . Str::slug($rubrik->rubrik_name) . '/sitemap_web.xml');
         }
+
         $sitemap->writeToFile(public_path('sitemap.xml'));
     }
 
     public function create_site()
     {
+
     }
 
-    public function create_web_post()
+    public function create_posts()
     {
         $rubriks = Rubrik::all();
         foreach ($rubriks as $rubrik) {
@@ -52,10 +75,7 @@ class SitemapController extends Controller
                 ->writeToFile(storage_path("app/{$folder_path}/sitemap_web.xml")); // simpan ke storage
 
         }
-    }
 
-    public function create_news_post()
-    {
         $rubriks = Rubrik::all();
         foreach ($rubriks as $rubrik) {
             $folder_name = Str::slug($rubrik->rubrik_name);
@@ -88,9 +108,47 @@ class SitemapController extends Controller
 
     public function create_amps()
     {
+        $rubriks = Rubrik::all();
+        foreach ($rubriks as $rubrik) {
+            // buat folder jika belum ada
+            $folder_name = Str::slug($rubrik->rubrik_name);
+            $folder_path = "public/sitemaps/amp/{$folder_name}";
+            Storage::makeDirectory($folder_path);
+
+            // generate sitemap
+            Sitemap::create()
+                ->add(Amp::where('status', 'published')
+                    ->where('category', $rubrik->rubrik_id)
+                    ->orderBy('published_at', 'desc')
+                    ->limit(200)
+                    ->get())
+                ->writeToFile(storage_path("app/{$folder_path}/sitemap_web.xml")); // simpan ke storage
+
+        }
+
+        $rubriks = Rubrik::all();
+        foreach ($rubriks as $rubrik) {
+            // buat folder jika belum ada
+            $folder_name = Str::slug($rubrik->rubrik_name);
+            $folder_path = "public/sitemaps/amp/{$folder_name}";
+            Storage::makeDirectory($folder_path);
+
+            // generate sitemap
+            Sitemap::create()
+                ->add(Amp::where('status', 'published')
+                    ->where('category', $rubrik->rubrik_id)
+                    ->orderBy('published_at', 'desc')
+                    ->limit(200)
+                    ->get())
+                ->writeToFile(storage_path("app/{$folder_path}/sitemap_web.xml")); // simpan ke storage
+
+        }
     }
 
     public function create_tags()
     {
+        $res = Sitemap::create()
+            ->add(Tags::all())
+            ->writeToFile(public_path('sitemap/tags.xml'));
     }
 }

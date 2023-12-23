54f969947d4f33383469f259deea855e898dc06e
irpannawawi <irpannawawi.ixd@gmail.com>
2023-12-23 08:42:16 +0700

    cdn to origin

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,8 +16,6 @@
     <meta name="msapplication-TileImage" content="{{ Storage::url('favicon/') . get_setting('favicon') }}" />
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
  
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])

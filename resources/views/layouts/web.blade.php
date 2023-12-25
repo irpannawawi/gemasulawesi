8f4a22a013049f59dadb4291de9e26782dfb98ef
irpannawawi <irpannawawi.ixd@gmail.com>
2023-12-26 01:28:49 +0700

    asyc defer

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -636,7 +636,7 @@
     <!-- The core Firebase JS SDK is always required and must be listed first -->
     <script src="https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js"></script>
     <script src="https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js"></script>
-    <script>
+    <script async defer>
         // Your web app's Firebase configuration
         const firebaseConfig = {
             apiKey: "{{ env('FIREBASE_API_KEY') }}",

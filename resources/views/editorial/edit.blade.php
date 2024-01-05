fa4ebd611ef7b1e0fc4c6b43c5b30ba734576d17
irpannawawi <irpannawawi.ixd@gmail.com>
2024-01-06 02:51:32 +0700

    update baca juga

--- resources/views/editorial/edit.blade.php
+++ resources/views/editorial/edit.blade.php
@@ -338,7 +338,7 @@
                 height: 480,
                 onMessage: (instance, data) => {
                     tinymce.activeEditor.execCommand('insertHTML', false,
-                        `<p><strong>Baca Juga: <a href="${data.data.url}" >${data.data.title}</a></strong></p>`);
+                        `<p class="baca-juga"><strong>Baca Juga: <br /><a href="${data.data.url}" >${data.data.title}</a></strong></p>`);
                     instance.close();
                 }
             };

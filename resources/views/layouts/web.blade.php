4c8b38039507718a69ac0c775d003f0f9f47e811
irpannawawi <irpannawawi.ixd@gmail.com>
2024-01-13 12:56:44 +0700

    firebase update

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -716,8 +716,31 @@
                 console.log(`Token Error :: ${err}`);
             });
         }
-
         initFirebaseMessagingRegistration();
+
+
+        const registerServiceWorker = async () => {
+            if ("serviceWorker" in navigator) {
+                try {
+                    const registration = await navigator.serviceWorker.register("/firebase-messaging-sw.js", {
+                        scope: "/",
+                    });
+                    if (registration.installing) {
+                        console.log("Service worker installing");
+                    } else if (registration.waiting) {
+                        console.log("Service worker installed");
+                    } else if (registration.active) {
+                        console.log("Service worker active");
+                    }
+                } catch (error) {
+                    console.error(`Registration failed with ${error}`);
+                }
+            }
+        };
+
+        // …
+
+        registerServiceWorker();
     </script>
     <!-- TODO: Add SDKs for Firebase products that you want to use
     https://firebase.google.com/docs/web/setup#available-libraries -->

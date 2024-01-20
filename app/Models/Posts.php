8eae2d4e975ec439e2c0719f1c05b9b1de70fe48
irpannawawi <irpannawawi.ixd@gmail.com>
2024-01-20 14:21:50 +0700

    update feed

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -101,7 +101,7 @@ class Posts extends Model implements Sitemapable, Feedable
     public static function getAllFeedItems()
     {
         return Posts::orderBy('published_at', 'desc')
-        ->limit(2)
+        ->limit(1)
         ->where('published_at', '>=', Carbon::now()->subHours(12)->toDateTimeString())
         ->get();
     }
